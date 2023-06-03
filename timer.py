32092cd8546e660c62aaa1de3cd2425e003dac95
johnmarco <johnmarcotasillo@gmail.com>
2023-06-03 15:50:32 -0400

    Fixed a bug where it could only find the sound files if you were in the file when calling the program

--- timer.py
+++ timer.py
@@ -1,10 +1,16 @@
 import math
 from playsound import playsound
 import time
+import os
+
 work_length = int(input("Work session length in minutes: ")) * 60
 break_length = int(input("Break session length in minutes: ")) * 60
 count_down = work_length
 timer_type = "Work"
+
+
+os.system('cls')
+current_path = os.path.dirname(os.path.abspath(__file__))
 
 
 def format_time(time):
@@ -20,7 +26,7 @@ def format_time(time):
 
 
 while True:
-    print("Time left: " + format_time(count_down), end="\r")
+    print(f'{timer_type} time left: {format_time(count_down)}', end="\r")
     time.sleep(0.1)
     count_down -= 1
 
@@ -28,8 +34,10 @@ while True:
         if timer_type == "Work":
             timer_type = "Break"
             count_down = break_length
-            playsound("bell1.mp3")
+            playsound(os.path.join(current_path, "bell2.mp3"))
+            os.system('cls')
         else:
             timer_type = "Work"
             count_down = work_length
-            playsound("bell2.mp3")
+            playsound(os.path.join(current_path, "bell1.mp3"))
+            os.system('cls')
